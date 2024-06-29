c539c894b091fa2d0ddeff0a0b902cab67ee3af3
Abdul <abdulshafey24@gmail.com>
2024-06-29 13:30:41 +0530

    Create AdminPanel page and complete functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
@@ -12,6 +12,7 @@ const Header = () => {
   const user = useSelector((state) => state?.user?.user);
   // console.log("Header", user);
   const dispatch = useDispatch();
+  const [menuDisplay, setMenuDisplay] = useState(false);
 
   const handleLogout = async () => {
     const fetchData = await fetch(`${SummaryApi.logout_user.url}`, {
@@ -51,15 +52,33 @@ const Header = () => {
         </div>
 
         <div className="flex items-center gap-7">
-          <div className="text-3xl cursor-pointer relative flex justify-center">
-            {user?.profilePic ? (
-              <img
-                src={user?.profilePic}
-                className="w-10 h-10 rounded-full"
-                alt={user?.name}
-              />
-            ) : (
-              <FaRegCircleUser />
+          <div className="relative flex justify-center">
+            <div
+              className="text-3xl cursor-pointer relative flex justify-center"
+              onClick={() => setMenuDisplay((preve) => !preve)}
+            >
+              {user?.profilePic ? (
+                <img
+                  src={user?.profilePic}
+                  className="w-10 h-10 rounded-full"
+                  alt={user?.name}
+                />
+              ) : (
+                <FaRegCircleUser />
+              )}
+            </div>
+
+            {menuDisplay && (
+              <div className="absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded">
+                <nav>
+                  <Link
+                    to={"admin-panel"}
+                    className=" whitespace-nowrap hover:bg-slate-100"
+                  >
+                    Admin Panel
+                  </Link>
+                </nav>
+              </div>
             )}
           </div>
           <div className="text-2xl relative">
