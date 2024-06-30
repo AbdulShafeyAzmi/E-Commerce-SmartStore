a8ae9eb16369287498c3ec05442b30a3d931fbdb
Abdul <abdulshafey24@gmail.com>
2024-06-30 15:18:46 +0530

    Create UploadProduct api and add imageUpload functionality using Cloudinary

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import UploadProduct from "../components/UploadProduct";
+// import SummaryApi from '../common'
+// import AdminProductCard from '../components/AdminProductCard'
 
 const AllProducts = () => {
-  return <div>AllProducts</div>;
+  const [openUploadProduct, setOpenUploadProduct] = useState(false);
+  // const [allProduct,setAllProduct] = useState([])
+
+  // const fetchAllProduct = async() =>{
+  //   const response = await fetch(SummaryApi.allProduct.url)
+  //   const dataResponse = await response.json()
+
+  //   console.log("product data",dataResponse)
+
+  //   setAllProduct(dataResponse?.data || [])
+  // }
+
+  // useEffect(()=>{
+  //   fetchAllProduct()
+  // },[])
+  return (
+    <div>
+      <div className="bg-white py-2 px-4 flex justify-between items-center">
+        <h2 className="font-bold text-lg">All Product</h2>
+        <button
+          className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full "
+          onClick={() => setOpenUploadProduct(true)}
+        >
+          Upload Product
+        </button>
+      </div>
+
+      {/**upload prouct component */}
+      {openUploadProduct && (
+        <UploadProduct
+          onClose={() => setOpenUploadProduct(false)}
+          // fetchData={fetchAllProduct}
+        />
+      )}
+    </div>
+  );
 };
 
 export default AllProducts;
