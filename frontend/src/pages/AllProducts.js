d308a25bd14c4777f25cd11a3a0695331ba970f5
Abdul <abdulshafey24@gmail.com>
2024-07-01 18:12:08 +0530

    Complete AllProducts page functionality

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 import UploadProduct from "../components/UploadProduct";
-// import SummaryApi from '../common'
-// import AdminProductCard from '../components/AdminProductCard'
+import SummaryApi from "../common";
+import AdminProductCard from "../components/AdminProductCard";
 
 const AllProducts = () => {
   const [openUploadProduct, setOpenUploadProduct] = useState(false);
-  // const [allProduct,setAllProduct] = useState([])
+  const [allProduct, setAllProduct] = useState([]);
 
-  // const fetchAllProduct = async() =>{
-  //   const response = await fetch(SummaryApi.allProduct.url)
-  //   const dataResponse = await response.json()
+  const fetchAllProduct = async () => {
+    const response = await fetch(SummaryApi.allProduct.url);
+    const dataResponse = await response.json();
 
-  //   console.log("product data",dataResponse)
+    //console.log("product data", dataResponse);
 
-  //   setAllProduct(dataResponse?.data || [])
-  // }
+    setAllProduct(dataResponse?.data || []);
+  };
 
-  // useEffect(()=>{
-  //   fetchAllProduct()
-  // },[])
+  useEffect(() => {
+    fetchAllProduct();
+  }, []);
   return (
     <div>
       <div className="bg-white py-2 px-4 flex justify-between items-center">
@@ -29,6 +29,19 @@ const AllProducts = () => {
         >
           Upload Product
         </button>
+      </div>
+
+      {/**all product */}
+      <div className="flex items-center flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll">
+        {allProduct.map((product, index) => {
+          return (
+            <AdminProductCard
+              data={product}
+              key={index + "allProduct"}
+              fetchdata={fetchAllProduct}
+            />
+          );
+        })}
       </div>
 
       {/**upload prouct component */}
