0365ce0b41fee2b812ffec2e325827e663e0e6a5
Abdul <abdulshafey24@gmail.com>
2024-07-01 18:06:16 +0530

    Create updateProduct api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,7 @@ const allUsers = require("../controller/user/allUsers");
 const updateUser = require("../controller/user/updateUser");
 const UploadProductController = require("../controller/product/uploadProduct");
 const getProductController = require("../controller/product/getProduct");
+const updateProductController = require("../controller/product/updateProduct");
 
 router.post("/signup", userSignUpController);
 router.post("/signin", signinController);
@@ -19,9 +20,10 @@ router.get("/userLogout", userLogout);
 //Admin Panel
 router.get("/all-user", authToken, allUsers);
 router.post("/update-user", authToken, updateUser);
-router.get("/get-product", getProductController);
 
 //Product
 router.post("/upload-product", authToken, UploadProductController);
+router.get("/get-product", getProductController);
+router.post("/update-product", authToken, updateProductController);
 
 module.exports = router;
