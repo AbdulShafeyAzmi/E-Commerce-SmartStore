9e1caffc84b46e3f62d94105f940e17fbe727695
Abdul <abdulshafey24@gmail.com>
2024-07-19 17:46:56 +0530

    Complete ProductDetails  and Home page functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SummaryApi from "./common";
 import Context from "./context";
 import { useDispatch } from "react-redux";
@@ -13,6 +13,7 @@ import { setUserDetails } from "./store/userSlice";
 
 function App() {
   const dispatch = useDispatch();
+  const [cartProductCount, setCartProductCount] = useState(0);
   const fetchUserDetails = async () => {
     const dataResponse = await fetch(SummaryApi.current_user.url, {
       method: SummaryApi.current_user.method,
@@ -26,8 +27,20 @@ function App() {
     // console.log("user-data", dataApi.data);
   };
 
+  const fetchUserAddToCart = async () => {
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url, {
+      method: SummaryApi.addToCartProductCount.method,
+      credentials: "include",
+    });
+
+    const dataApi = await dataResponse.json();
+
+    setCartProductCount(dataApi?.data?.count);
+  };
+
   useEffect(() => {
     fetchUserDetails();
+    fetchUserAddToCart();
   }, []);
   return (
     <>
